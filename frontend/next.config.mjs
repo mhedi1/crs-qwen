d3262e316b07bf4a79f2ec090c60ae988ea89b4e
Mohamed Hedi Foughali <mh.foughali@pristini-international.tn>
2026-07-12 23:57:30 +0200

    Point Next.js proxy to university server

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
+        destination: 'http://sinbad2ia.ujaen.es:5000/api/:path*',
       }
     ]
   }
